384dbd136cb337d2f3aad7a45d48b6bf814d391d
zhangxirui <2919737573@qq.com>
2020-02-15 18:23:27 +0800

    权限处理成果

---
+++ app/src/main/java/com/example/luckzhang/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.luckzhang;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+/*在本界面进行权限获取
+* 以及界面的初始化
+* */
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG="MainActivity.this";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getPermissions();
+    }
+
+    //获取权限，下面是回调
+    private void getPermissions(){
+        String[] permissions={Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+        for(int i=0;i<=2;i++){
+            if(ContextCompat.checkSelfPermission(MainActivity.this,permissions[i])
+                    != PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG,"11111111111111:"+permissions[i]);
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        permissions,1);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull final String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1){
+            for(int ii=0;ii<=2;ii++){
+                if(grantResults.length>0&&grantResults[ii]!=PackageManager.PERMISSION_GRANTED){
+                    AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("提示");
+                    switch (ii){
+                        case 0:
+                            builder.setMessage("如果不授予相机权限，我们将无法进行体态测试");
+                            break;
+                        case 1:
+                            builder.setMessage("如果不授予读写权限，我们将无法处理数据");
+                            break;
+                        case 2:
+                            builder.setMessage("如果不授予定位权限，我们将无法进行数据统计");
+                            break;
+                    }
+                    final int finalIi = ii;
+                    builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{permissions[finalIi]},2);
+                        }
+                    });
+                    builder.create().show();
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/luckzhang/Welcome_Activity.java
+++ app/src/main/java/com/example/luckzhang/Welcome_Activity.java
@@ -1,19 +1,137 @@
 package com.example.luckzhang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import LoginAndRegister.LoginActivity;
 
 /*欢迎界面
 * 这是用户看到的第一个界面
-* 可以放一些图片或者其他的什么的东西，预计2秒中
+* 可以放一些图片或者其他的什么的东西，预计3秒中
+* 如果已经登录，送到MainActivty，
+* 未登录送到login
 * */
 
 public class Welcome_Activity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView1,imageView2;
+    private static final int UPDATE_BUTTON_TEXT=1;
+    private MyThread myThread;
+    private AdvertisementDownLoad advertisementDownLoad;
+    private boolean judge=true;
+    final private String TAG="Welcome_Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initThis();
+
     }
+
+    private void initThis(){
+        button=findViewById(R.id.button);
+        imageView1=findViewById(R.id.imageView);
+        imageView2=findViewById(R.id.imageView2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leaveThis();
+            }
+        });
+
+        myThread=new MyThread();
+        new Thread(myThread).start();
+
+        advertisementDownLoad=new AdvertisementDownLoad();
+        new Thread(advertisementDownLoad).start();
+    }
+
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            if(msg.what==1){
+                button.setText("跳过(1s)");
+            }else if(msg.what==2){
+                button.setText("跳过(2s)");
+            }else if(msg.what==0&&judge){
+                leaveThis();
+            }
+        }
+    };
+
+    //3秒倒数子线程
+    class MyThread implements Runnable{
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message=new Message();
+            message.what=2;
+            handler.sendMessage(message);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message1=new Message();
+            message1.what=1;
+            handler.sendMessage(message1);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message2=new Message();
+            message2.what=0;
+            handler.sendMessage(message2);
+        }
+    }
+
+
+    //广告界面下载主线程，此页面用于下次打开时使用
+    //跳转后子线程也会继续运行
+    class AdvertisementDownLoad implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }
+
+    //准备离开此页面
+    private void leaveThis(){
+        judge=false;
+        //在这里判断是否登录，未登录直接送到login界面
+        Intent intent=new Intent();
+        intent.setClass(Welcome_Activity.this, MainActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+
 }
